e413046654b4802cc653c36897d51e5cbfbad849
hdy86 <dyhong86@naver.com>
2022-10-12 05:26:24 +0900

    add recoil, category select

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,33 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+export enum Categories {
+  "TO_DO",
+  "DOING",
+  "DONE",
+}
 
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
+
+export const categoryState = atom<Categories>({
+  key: "category",
+  default: Categories.TO_DO,
+});
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    // const toDos = get(toDoState); // toDoState atom 을 바라보고 있음
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
